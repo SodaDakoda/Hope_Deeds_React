2efe2dc76f52e6ce4d028b7483ebce208bd89343
Mac <33965646+SodaDakoda@users.noreply.github.com>
2025-12-02 16:01:45 -0800

    i did something lol i forgot

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useState, useEffect } from "react";
-import { getProfile } from "../services/auth";
+import { getOrgProfile as getProfile } from "../services/auth";
 
 const AuthContext = createContext();
 
